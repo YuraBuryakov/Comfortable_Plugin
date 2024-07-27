769c63dab897ef7b828d2e03c8f5d4b1b7e61c8d
YuraBuryakov <yuraburyakov@gmail.com>
2024-07-27 13:49:22 +0200

    added 2 plugins

---
+++ main_project_script/main.js
@@ -0,0 +1,8 @@
+import { smalltime } from "../simple-calendar";
+
+console.log("Activatong smalltime");
+smalltime();
+console.log("smalltime is setted and ready")
+// Hooks.on('init', () => {
+//  console.log("main init")
+// });
